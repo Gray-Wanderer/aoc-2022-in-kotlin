70cba5a7707d085e4b28fe2826986bf08a47813b
andre <andrey.mish@gmail.com>
2022-12-19 22:02:53 +0100

    Adventofcode day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
+class CalorieCounting : StringAcceptor {
+    var sum = 0
+    override fun applyString(input: String): Boolean {
+        return if (input.isNotBlank()) {
+            sum += input.toInt()
+            false
+        } else {
+            true
+        }
+    }
+}
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return transform(input) { CalorieCounting() }.maxOfOrNull(CalorieCounting::sum) ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sums = transform(input) { CalorieCounting() }.map(CalorieCounting::sum).sortedDescending()
+        return sums[0] + sums[1] + sums[2]
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,29 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T> transform(input: List<String>, acceptorCreator: () -> T): List<T>
+    where T : StringAcceptor {
+    val out = mutableListOf<T>()
+
+    var acceptor: T? = null
+    for (s in input) {
+        if (acceptor == null) {
+            acceptor = acceptorCreator()
+        }
+        acceptor.let {
+            if (it.applyString(s)) {
+                out.add(it)
+                acceptor = null
+            }
+        }
+    }
+
+    acceptor?.let { out.add(it) }
+
+    return out
+}
+
+interface StringAcceptor {
+    fun applyString(input: String): Boolean
+}
