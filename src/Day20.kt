23d37ecc35e08f8dcf4914fd9ec42c2c13340c49
andre <andrey.mish@gmail.com>
2022-12-21 00:22:51 +0100

    Adventofcode day 20

---
+++ src/Day20.kt
@@ -0,0 +1,82 @@
+import kotlin.math.abs
+
+class Node(val number: Long, nodeCount: Int) {
+    var prew: Node = this
+    var next: Node = this
+    val move: Long
+
+    init {
+        var truncatedMove = abs(number)
+        while (truncatedMove >= nodeCount) {
+            truncatedMove = (truncatedMove % nodeCount) + (truncatedMove / nodeCount)
+        }
+        this.move = if (number > 0) truncatedMove else -truncatedMove
+    }
+
+    fun addNode(node: Node) {
+        val nextNode = this.next
+        this.next = node
+        nextNode.prew = node
+        node.next = nextNode
+        node.prew = this
+    }
+}
+
+fun moveNode(node: Node) {
+    val steps = abs(node.move)
+
+    if (steps == 0L) {
+        return
+    }
+
+    var rightNode = node.prew
+
+    node.prew.next = node.next
+    node.next.prew = node.prew
+
+    for (i in 1..steps) {
+        rightNode = if (node.move < 0) rightNode.prew else rightNode.next
+    }
+
+    rightNode.addNode(node)
+}
+
+tailrec fun getForIndex(head: Node, index: Int): Node {
+    return if (index == 0) head else getForIndex(head.next, index - 1)
+}
+
+fun main() {
+    fun solution(input: List<String>, multiplyer: Long = 1, iterations: Int = 1): Long {
+        val allNodes = input.map {
+            Node(it.toLong() * multiplyer, input.size)
+        }
+        var head = allNodes.find { it.number == 0L } ?: allNodes[0]
+        var tail = allNodes[0]
+
+        allNodes.forEach {
+            tail.addNode(it)
+            tail = it
+        }
+
+        for (i in 1..iterations) {
+            for (node in allNodes) {
+                moveNode(node)
+            }
+        }
+
+        var sum = 0L
+        for (i in 1..3) {
+            head = getForIndex(head, 1000)
+            sum += head.number
+        }
+        return sum
+    }
+
+    val testInput = readInput("Day20_test")
+    check(solution(input = testInput) == 3L)
+    check(solution(input = testInput, multiplyer = 811589153L, 10) == 1623178306L)
+
+    val input = readInput("Day20")
+    solution(input).println()
+    solution(input, multiplyer = 811589153L, iterations = 10).println()
+}
