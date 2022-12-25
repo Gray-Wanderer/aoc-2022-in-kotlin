6343ded181a5e6cd37c84b94fde9965ae4deb201
Gray-Wanderer <andrey.mish@gmail.com>
2022-12-25 15:02:32 +0100

    Adventofcode day 23

--- src/Utils.kt
+++ src/Utils.kt
@@ -45,7 +45,29 @@ fun <T> transform(input: List<String>, acceptorCreator: () -> T): List<T>
     return out
 }
 
-class Point(val x: Int, val y: Int)
+class Point(val x: Int, val y: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Point
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        return result
+    }
+
+    override fun toString(): String {
+        return "Point(x=$x, y=$y)"
+    }
+}
 
 interface StringAcceptor {
     fun applyString(input: String): Boolean
